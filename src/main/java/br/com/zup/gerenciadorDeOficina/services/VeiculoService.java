5268704b0ee68c8340230c910580a9c4c21c24f5
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-17 14:25:31 -0300

    Criar serviço de veículo

---
+++ src/main/java/br/com/zup/gerenciadorDeOficina/services/VeiculoService.java
@@ -0,0 +1,13 @@
+package br.com.zup.gerenciadorDeOficina.services;
+
+import br.com.zup.gerenciadorDeOficina.models.Veiculo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VeiculoService {
+
+    private List<Veiculo> veiculos = new ArrayList<>();
+
+
+}
