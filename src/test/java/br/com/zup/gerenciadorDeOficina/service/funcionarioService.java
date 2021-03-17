e6a8df09af7a1542cc58ff99d0f37191c67b0e21
Kaio <kaio.moraes@zup.com.br>
2021-03-17 14:42:56 -0300

    Criar método de cadastro de funcionário

--- src/test/java/br/com/zup/gerenciadorDeOficina/service/funcionarioService.java
+++ src/test/java/br/com/zup/gerenciadorDeOficina/service/funcionarioService.java
@@ -1,4 +1,21 @@
 package br.com.zup.gerenciadorDeOficina.service;
 
+import br.com.zup.gerenciadorDeOficina.models.Funcionario;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class funcionarioService {
+
+    private static List<Funcionario> funcionarios =new ArrayList<>();
+
+    public Funcionario cadastrarFuncionario(Funcionario funcionario){
+        funcionarios.add(funcionario);
+        return funcionario;
+
+    }
+
+
 }
