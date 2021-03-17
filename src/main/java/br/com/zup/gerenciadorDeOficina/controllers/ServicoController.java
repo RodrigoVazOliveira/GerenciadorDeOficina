46b7ea73974af156528925745f53f452c532be7e
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-17 17:17:18 -0300

    Criar método com endpoint servicos/ POST para cadastrar um serviço no controller de serviço

--- src/main/java/br/com/zup/gerenciadorDeOficina/controllers/ServicoController.java
+++ src/main/java/br/com/zup/gerenciadorDeOficina/controllers/ServicoController.java
@@ -1,9 +1,10 @@
 package br.com.zup.gerenciadorDeOficina.controllers;
 
+import br.com.zup.gerenciadorDeOficina.models.Servico;
 import br.com.zup.gerenciadorDeOficina.services.ServicoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/servicos/")
@@ -11,4 +12,10 @@ public class ServicoController {
 
     @Autowired
     private ServicoService servicoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Servico cadastrarServico(@RequestBody Servico servico) {
+        return servicoService.cadatrar(servico);
+    }
 }
