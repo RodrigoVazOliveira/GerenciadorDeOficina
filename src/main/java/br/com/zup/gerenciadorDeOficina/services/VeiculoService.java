4430a64052fe8a9275a94b13f743319b5cc0e14c
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-17 14:29:25 -0300

    Criar método de cadastrar veículo no serviço de veículo

--- src/main/java/br/com/zup/gerenciadorDeOficina/services/VeiculoService.java
+++ src/main/java/br/com/zup/gerenciadorDeOficina/services/VeiculoService.java
@@ -1,13 +1,19 @@
 package br.com.zup.gerenciadorDeOficina.services;
 
 import br.com.zup.gerenciadorDeOficina.models.Veiculo;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class VeiculoService {
 
     private List<Veiculo> veiculos = new ArrayList<>();
 
+    public Veiculo cadastrar(Veiculo veiculo) {
+        veiculos.add(veiculo);
 
+        return veiculo;
+    }
 }
