43b9e7b01ad748a57456ae1f624775c064977a01
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-18 16:24:32 -0300

    Criar método para obter validações

--- src/main/java/br/com/zup/gerenciadorDeOficina/exceptions/ManipuladorDeExcecao.java
+++ src/main/java/br/com/zup/gerenciadorDeOficina/exceptions/ManipuladorDeExcecao.java
@@ -8,11 +8,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class ManipuladorDeExcecao extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    }
+
+    private List<ObjetoDeErro> getErros(MethodArgumentNotValidException ex) {
+        List<ObjetoDeErro> objetosDeErro = ex.getBindingResult().getFieldErrors()
+                .stream().map(error -> new ObjetoDeErro(error.getDefaultMessage(), error.getField()))
+                .collect(Collectors.toList());
+        return objetosDeErro;
     }
 }
