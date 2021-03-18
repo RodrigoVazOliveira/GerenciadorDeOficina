d1ed1ff9030067d32af27b8e707ba18f9bbdfc02
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-18 15:51:06 -0300

    Criar manipulador de exceção

---
+++ src/main/java/br/com/zup/gerenciadorDeOficina/exceptions/ManipuladorDeExcecao.java
@@ -0,0 +1,9 @@
+package br.com.zup.gerenciadorDeOficina.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ManipuladorDeExcecao extends ResponseEntityExceptionHandler {
+    
+}
