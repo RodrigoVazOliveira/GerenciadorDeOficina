bbf62fc5f777f67ece003fa1daaaed84e417ff28
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-18 17:03:59 -0300

    Criar validação na classe de serviço do cadastro do veículo com exceção personalizada

--- src/main/java/br/com/zup/gerenciadorDeOficina/services/VeiculoService.java
+++ src/main/java/br/com/zup/gerenciadorDeOficina/services/VeiculoService.java
@@ -1,5 +1,6 @@
 package br.com.zup.gerenciadorDeOficina.services;
 
+import br.com.zup.gerenciadorDeOficina.exceptions.VeiculoDuplicadoExcecao;
 import br.com.zup.gerenciadorDeOficina.models.Veiculo;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
@@ -11,16 +12,26 @@ public class VeiculoService {
     private List<Veiculo> veiculos = new ArrayList<>();
 
     public Veiculo cadastrar(Veiculo veiculo) {
+        if (veiculos.contains(veiculo)) {
+            throw new VeiculoDuplicadoExcecao("o veículo com chassi " + veiculo.getChassi() + " já existe no sistema!");
+        }
         veiculos.add(veiculo);
         return veiculo;
     }
 
     public Veiculo pesquisarChassi(String chassi) throws RuntimeException {
+        Veiculo resultadoVeiculo = null;
         for (Veiculo veiculo : veiculos) {
             if (veiculo.getChassi().equals(chassi)) {
-                return veiculo;
+                resultadoVeiculo = veiculo;
             }
-        }throw new RuntimeException("Chassi não encontrado");
+        }
+
+        if (resultadoVeiculo == null) {
+            throw new RuntimeException("Chassi não encontrado");
+        }
+
+        return resultadoVeiculo;
     }
 
 }
